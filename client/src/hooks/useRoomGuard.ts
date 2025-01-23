4d06804d359a12fb18ea1d72b094f428f32ec2bd
Nils Siemsen <n.siemsen@right-basedonscience.de>
2025-01-23 15:05:16 +0100

    makes fronted use prefix

--- client/src/hooks/useRoomGuard.ts
+++ client/src/hooks/useRoomGuard.ts
@@ -21,7 +21,7 @@ export function useRoomGuard(): boolean {
             navigate(`/join?room=${room}`)
         }
 
-        fetch(`${import.meta.env.VITE_API_URL}/${import.meta.env.WS_PREFIX}/auth/validate`, {
+        fetch(`${import.meta.env.VITE_API_URL}/${import.meta.env.VITE_WS_PREFIX}/auth/validate`, {
             headers: {
                 "Authorization": `Bearer ${token}`
             }
